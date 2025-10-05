33813386c36fd1cb9d7559e3ed8ee8de2a3efc99
Zsolt <zsolt.baranyosgmail.com>
2025-10-05 12:29:25 +0200

    more changes

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,22 +1,13 @@
+import sqlite3
 from app.database import Base, engine
 from app import models
-import pyodbc
-
-conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=localhost;'
-    'DATABASE=edu_platform;'
-    'UID=sa;'
-    'PWD=edu_platform'
-)
-print("Kapcsolódás sikeres!")
-conn.close()
 
 # Táblák létrehozása az adatbázisban
-#print("Táblák létrehozása az SQL Serverben...")
+#print("Táblák létrehozása az SQLite-ban...")
 #Base.metadata.create_all(bind=engine)
 #print("✅ Kész! Minden tábla létrehozva.")
 
-
-
-
+conn = sqlite3.connect("local_db.sqlite3")
+cursor = conn.execute("SELECT * FROM users")
+print("Felhasznalok listaja: ", cursor.fetchall())
+conn.close()
