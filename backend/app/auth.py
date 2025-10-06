cef9492874080d96ad5fa7ff3499c6037682a7f7
Zsolt <zsolt.baranyosgmail.com>
2025-10-06 16:25:05 +0200

    Refactor authentication settings to use configuration values for SECRET_KEY, ALGORITHM, and ACCESS_TOKEN_EXPIRE_MINUTES

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -8,17 +8,13 @@ from app.database import get_db
 from app.models import User  # vagy Student, ha így hívod
 from app.config import settings
 
-
-SECRET_KEY = "supersecretkey"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
-
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = settings.SECRET_KEY
-ALGORITHM = "HS256"
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
